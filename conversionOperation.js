04ac68975b8671363f444154c5b136f5ad88b433
Navinya-30 <navinyaghanekar30@gmail.com>
2026-04-08 21:41:03 +0530

    Lecture 1 Operations and their conditions

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,110 +1,154 @@
-let score = 33
-console.log(typeof score);
-console.log(typeof(score));
-//output is number
+// let score = 33
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// //output is number
 
 
-let score = "33"
-console.log(typeof score);
-console.log(typeof(score)); 
-// output is string
+// let score = "33"
+// // console.log(typeof score);
+// // console.log(typeof(score)); 
+// // output is string
 
 
-let score ="33"
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-// output is number
+// let score ="33"
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // output is number
 
 
-let score ="33abc"
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-// output is number
+// let score ="33abc"
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // output is number
 
-console.log(valueInNumber)
-//output is NaN(Not a Number)
-
-
-let score = null
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-//output is 0
+// // console.log(valueInNumber)
+// //output is NaN(Not a Number)
 
 
-let score = undefined
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-// output is NaN
+// let score = null
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);
+// //output is 0
 
 
-let score = true;
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-//output is 1
+// let score = undefined
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);
+// // output is NaN
 
 
-let score = "Navinya";
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-//output is NaN
+// let score = true;
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);
+// //output is 1
 
 
-Conversion in Number
-
-"33" => 33
-"33abc" => NaN
-true => 1; false => 0
-
-
-let isLoggedIn = true
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-// output is true
+// let score = "Navinya";
+// // console.log(typeof score);
+// // console.log(typeof(score));
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);
+// //output is NaN
 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-// output is true
+// // Conversion in Number
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
 
-let isLoggedIn = 0
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-// output is false
+// let isLoggedIn = true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
+// // output is true
 
 
-let isLoggedIn = "";
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-// output is false
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+// // output is true
 
 
-let isLoggedIn = "Navinya";
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-// output is true
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+// // output is false
 
 
-1 => true; 0 => false
-"" => false;
-"Navinya" => true
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+// // output is false
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
-// output is 33 and string
+// let isLoggedIn = "Navinya";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+// // output is true
 
+
+// // 1 => true; 0 => false
+// // "" => false;
+// // "Navinya" => true
+
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+// // output is 33 and string
+
+
+//****************OPERATIONS**************//
+
+// console.log(5+4);
+// //output is 9
+// console.log(5-4);
+// //output is 1
+// console.log(5*4);
+// //output is 20
+// console.log(5/4);
+// //output is 1.25
+// console.log(5%4);
+// //output is 1
+// console.log(5**4);
+//output is 625
+
+
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
+// // output is -3
+
+
+// let str1 = "Navinya"
+// let str2 = "Vedant"
+// let str3 = (str1+str2);
+// // console.log(str3);
+// // NavinyaVedant
+
+
+// console.log(1+"1");
+// // output is 11
+// console.log("1"+2);
+// // output is 12
+// console.log("1"+2+3);
+// // output is 123
+// console.log(1+2+"3");
+// // output is 33
+
+// console.log(+true);
+// // output is 1
+
+// console.log(+"")
+// // output is 0
 
