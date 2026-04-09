ccfb21173acf00482c31b0e4d96c098da6b6e1e1
Navinya-30 <navinyaghanekar30@gmail.com>
2026-04-09 22:28:46 +0530

    JS lecture 2 : comparisonOfDatatypes

---
+++ comaprisonsOfDatatypes.js
@@ -0,0 +1,24 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//comparioson of same datatype
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0)
+// console.log(null == 0);
+// console.log(null>= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//comparison of different data type 
+
+// console.log("2"===2);
+
+//comparison of diff datatype but with strict check
