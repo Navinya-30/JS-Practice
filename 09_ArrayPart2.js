b2a98ac0a5b83b5109cad909adef39c6f2bc7f1a
Navinya-30 <navinyaghanekar30@gmail.com>
2026-04-23 13:59:01 +0530

    Js lecture 9 and 10 learning with arrays and objects

--- 09_ArrayPart2.js
+++ 09_ArrayPart2.js
@@ -3,8 +3,11 @@ const dc_heros = ["superman", "flash", "batman"]
 
 //marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[2][1]);
+console.log(marvel_heros);
+// [ 'thor', 'Spiderman', 'Ironman' ]
+
+console.log(marvel_heros[2][1]);
+// r
 
 const allHeros = marvel_heros.concat(dc_heros)
 console.log(allHeros);
@@ -12,6 +15,7 @@ console.log(allHeros);
 
 const all_new_heros = [...marvel_heros,...dc_heros]
 console.log(all_new_heros)
+// [ 'thor', 'Spiderman', 'Ironman', 'superman', 'flash', 'batman' ]
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_another_array = another_array.flat(Infinity)
---
+++ 10_objects.js
@@ -0,0 +1,16 @@
+// singleton
+// Object.create
+
+// object literals
+const JsUser = {
+    name : "Navinya",
+    age : 19,
+    location : "Nagpur",
+    email: "navinya@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
